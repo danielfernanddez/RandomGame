ef0f1bbb323d9739fa149430486f84e518e8a60c
Daniel Fernández Jaime <danielfernandddez@gmail.com>
2023-12-20 17:09:36 +0100

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let numeroAleatorio = "";//Declaramos una variable vacía que contendrá el número aleatorio.
+let btnComprobar = document.querySelector("#comprobar");//Declaramos una variable que contenga el botón del HTML que va a comprobar el número.
+let btnBorrarDatos = document.querySelector(".borrarDatos");//Declaramos una variable que contenga el botón del HTML que va a limpiar el localStorage.
+let textoResultado = document.querySelector("#resultado");//Declaramos una variable que contenga el div del HTML que tendrá el texto de resultado.
+let textoEstadisticas = document.querySelector("#estadisticas");//Declaramos una variable que contenga el div del HTML que tendrá el texto de estadísticas
