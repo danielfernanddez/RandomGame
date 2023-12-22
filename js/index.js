5735c78315986847d3a563b9b2a9dab8306ee013
Daniel Fernández Jaime <danielfernandddez@gmail.com>
2023-12-22 16:36:15 +0100

    cambio

--- js/index.js
+++ js/index.js
@@ -14,26 +14,26 @@ let intentoFallido = localStorage.getItem("fallidos") || 0;//Declaramos una vari
 
 function generarNumeroAleatorio(){ //Declaramos una función que generará el número aleatorio.
 
-    numeroAleatorio = Math.round(Math.random(10))//Le añade como valor a la variable que va a contener dicho número un random como número entero.
+    numeroAleatorio = Math.round(Math.random()*10)//Le añade como valor a la variable que va a contener dicho número un random como número entero.
 }
 
 //Función verificadora de número
 
 function comprobarNumero(){ //Declaramos una función que comprobará el número que agreguemos.
 
-    let inputNumero = document.querySelector("#numeroUsuario")//Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
-    parseInt(inputNumero)//Ese valor lo transformamos en un número entero (parseInt).
+    let inputNumero = document.querySelector("#numeroUsuario").value;//Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
+    inputNumero = parseInt(inputNumero)//Ese valor lo transformamos en un número entero (parseInt).
     if(inputNumero === numeroAleatorio){ //Si el número del usuario es estrictamente igual que el número almacenado en la variable que contiene el número aleatorio:
         intentoExito++//Aumentamos el valor de la variable que contiene el número de éxitos.
         localStorage.setItem("exitosos", intentoExito)//Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
         textoResultado.innerHTML = "¡Muy Bien!"//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "¡Muy bien!".
-        textoEstadisticas.innerHTML = `Éxitos: ${numeroAleatorio}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+        textoEstadisticas.innerHTML = `Éxitos: ${intentoExito}, Fallidos: ${intentoFallido}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
         generarNumeroAleatorio()//Llamamos a la función generadora de números para generar un nuevo número aleatorio.
     } else{
         intentoFallido++//Aumentamos el valor de la variable que contiene el número de fallidos.
         localStorage.setItem("fallidos", intentoFallido)//Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de fallidos.
         textoResultado.innerHTML = "Fallaste"//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "Fallaste".
-        textoEstadisticas.innerHTML = `Éxitos: ${numeroAleatorio}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+        textoEstadisticas.innerHTML = `Éxitos: ${intentoExito}, Fallidos: ${intentoFallido}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
     }
 }
 
@@ -56,6 +56,8 @@ btnBorrarDatos.addEventListener("click", limpiarDatos)//Creamos un addEventListe
 
 //Códigos de inicio
 
-textoEstadisticas.innerHTML = `Éxitos: ${numeroAleatorio}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+textoEstadisticas.innerHTML = `Éxitos: ${intentoExito}, Fallidos: ${intentoFallido}`//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
 
 generarNumeroAleatorio()//Llamamos la función generadora de números para que nos genere el primer número que el usuario debe adivinar.
+
+// guardo cambios
